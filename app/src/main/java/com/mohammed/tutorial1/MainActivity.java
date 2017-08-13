56cb51aaba8338616d05ce46fc6eaff809270c8b
Mohammed <abotammam1@googlemail.com>
2017-08-14 01:11:46 +0300

    The extraMessage value (inside the MainActivity.java) was edited

--- app/src/main/java/com/mohammed/tutorial1/MainActivity.java
+++ app/src/main/java/com/mohammed/tutorial1/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-public static final String extraMessage = "com.abotameem.tutorial1.extraMessage";
+public static final String extraMessage = "com.abotameem.tutorial1.Message";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
